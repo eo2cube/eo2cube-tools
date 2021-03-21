3427adff0352d72aa5c25776f1610b4d47ea97c1
Steve <steven.hill@uni-wuerzburg.de>
2021-03-21 18:47:22 +0100

    updated slc_mask function and removed odc-algo because of errors

--- eo2cube_tools/mask.py
+++ eo2cube_tools/mask.py
@@ -1,4 +1,6 @@
-import odc.algo
+import numpy as np
+import xarray as xr
+import dask
 
 scl_cat = {'1': 'Saturated or defective pixel',
     '2': 'Dark features / Shadows',
@@ -12,6 +14,48 @@ scl_cat = {'1': 'Saturated or defective pixel',
     '10': 'Thin cirrus',
     '11': 'Snow or ice'}
 
+
+def np_keep(array, mask, nodata, out_array=None):
+    if out_array is None:
+        out_array = np.full_like(array, nodata)
+    else:
+        assert out_array.shape == array.shape
+        assert out_array.dtype == array.dtype
+        assert out_array is not array
+        out_array[:] = nodata
+    np.copyto(out_array, array, where=mask)
+    return out_array
+
+def mask_values(ds, mask, inplace=False, nodata=None):
+
+    if isinstance(ds, xr.Dataset):
+        return ds.map(
+            lambda ds: mask_values(ds, mask, inplace=inplace), keep_attrs=True)
+
+    assert ds.shape == mask.shape
+    if nodata is None:
+        nodata = getattr(ds, "nodata", 0)
+
+    if inplace:
+        if dask.is_dask_collection(ds):
+            raise ValueError("Can not perform inplace operation on a dask array")
+
+        np.copyto(ds.data, nodata, where=~mask.data)
+        return ds
+
+    if dask.is_dask_collection(ds):
+        data = da.map_blocks(
+            keep_good_np,
+            ds.data,
+            where.data,
+            nodata,
+            name=randomize("good_pixels"),
+            dtype=ds.dtype,
+        )
+    else:
+        data = np_keep(ds.data, mask.data, nodata)
+
+    return xr.DataArray(data, dims=ds.dims, coords=ds.coords, attrs=ds.attrs, name=ds.name)
 
 def slc_mask(dataset, categories = ['Dark features / Shadows','Vegetation', 'Not vegetated', 'Water', 'Unclassified', 'Snow or ice']):
     """
@@ -37,4 +81,4 @@ def slc_mask(dataset, categories = ['Dark features / Shadows','Vegetation', 'Not
 
     mask = dataset["scl"].isin([int(k) for k,v in scl_cat.items() if v in categories])
 
-    return odc.algo.keep_good_only(dataset, where=mask)
+    return mask_values(dataset, mask=mask)
