7b4d884496cc58c42f0f9eca43c8cf002b5935fe
Steve <steven.hill@uni-wuerzburg.de>
2021-03-22 16:10:54 +0100

    simplified slc mask function

--- eo2cube_tools/mask.py
+++ eo2cube_tools/mask.py
@@ -15,52 +15,9 @@ scl_cat = {'1': 'Saturated or defective pixel',
     '11': 'Snow or ice'}
 
 
-def np_keep(array, mask, nodata, out_array=None):
-    if out_array is None:
-        out_array = np.full_like(array, nodata)
-    else:
-        assert out_array.shape == array.shape
-        assert out_array.dtype == array.dtype
-        assert out_array is not array
-        out_array[:] = nodata
-    np.copyto(out_array, array, where=mask)
-    return out_array
-
-def mask_values(ds, mask, inplace=False, nodata=None):
-
-    if isinstance(ds, xr.Dataset):
-        return ds.map(
-            lambda ds: mask_values(ds, mask, inplace=inplace), keep_attrs=True)
-
-    assert ds.shape == mask.shape
-    if nodata is None:
-        nodata = getattr(ds, "nodata", 0)
-
-    if inplace:
-        if dask.is_dask_collection(ds):
-            raise ValueError("Can not perform inplace operation on a dask array")
-
-        np.copyto(ds.data, nodata, where=~mask.data)
-        return ds
-
-    if dask.is_dask_collection(ds):
-        data = da.map_blocks(
-            keep_good_np,
-            ds.data,
-            where.data,
-            nodata,
-            name=randomize("good_pixels"),
-            dtype=ds.dtype,
-        )
-    else:
-        data = np_keep(ds.data, mask.data, nodata)
-
-    return xr.DataArray(data, dims=ds.dims, coords=ds.coords, attrs=ds.attrs, name=ds.name)
-
 def scl_mask(dataset, categories = ['Dark features / Shadows','Vegetation', 'Not vegetated', 'Water', 'Unclassified', 'Snow or ice']):
     """
-    Takes an xarray dataset, creates a mask based on categories defined in the SCL band and removes
-    all Pixels not included in the specified categories.
+    Takes an xarray dataset and creates a mask based on categories defined in the SCL band
 
     Parameters
     ----------
@@ -74,11 +31,9 @@ def scl_mask(dataset, categories = ['Dark features / Shadows','Vegetation', 'Not
 
     Returns
     -------
-    An xarray dataset only including pixels of the specified classes
+    An xarray dataset containing a mask for each time step
     """
 
     assert "scl" in list(dataset.data_vars.keys()), "scl band is missing"
 
-    mask = dataset["scl"].isin([int(k) for k,v in scl_cat.items() if v in categories])
-
-    return mask_values(dataset, mask=mask)
+    return dataset["scl"].isin([int(k) for k,v in scl_cat.items() if v in categories])
