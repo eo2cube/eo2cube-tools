b65287becbe76130e87ecbcf661c54fa9a20bf85
Ka Hei Chow <pinkychow1010@gmail.com>
2021-11-15 14:58:23 +0100

    update cloud mask ssmask (version 3)

--- eo2cube_tools/cloud_mask.py
+++ eo2cube_tools/cloud_mask.py
@@ -41,7 +41,259 @@ from scipy.stats import norm, kurtosis
 from scipy.signal import find_peaks
 from scipy.signal import argrelmin
 
-def ssmask_snow(dataset,strength=0.4):
+def ssmask_v3(dataset):
+    """
+    Adding cloud variable to detect cloud cover in the single scene.
+    Description
+    ----------
+    Generating cloud mask.
+    Parameters
+    ----------
+    dataset: xr.Dataset
+        dataset with a single time step, including bands "water_vapour","red","green","blue","nir", and "swir1". 
+        For multiple time steps, use s2cmask() instead.
+    Returns
+    -------
+    dataset: xr.DataArray
+        A xr.DataArray with variable "mask" of values 0, 6, 9, 11, or nan (0 = land, 6 = water, 9 = cloud, 11 = snow, nan = defect scene).
+    """
+    
+    # error catching    
+    assert isinstance(dataset, xr.Dataset),"Input has to be a xarray.Dataset."
+    
+    try:
+        dataset.red
+        dataset.green
+        dataset.blue
+        dataset.nir
+        dataset.swir1
+    except Exception:
+        print("RGB/NIR/SWIR1 bands not found.")
+    
+    try:
+        dataset.water_vapour
+    except Exception:
+        print("water_vapour band not found.")
+    
+    strength = 0.4
+    
+    # storing variable for defect bands
+    # if half or more of all pixels are zeros or missing values, the scene is treated as defect
+    numr = np.nan_to_num(dataset.red.values.flatten(), nan = 0.0)
+    numg = np.nan_to_num(dataset.green.values.flatten(), nan = 0.0)
+    numb = np.nan_to_num(dataset.blue.values.flatten(), nan = 0.0)
+    defectr = (np.median(numr) == 0)
+    defectg = (np.median(numg) == 0)
+    defectb = (np.median(numb) == 0)
+    
+    # for defect scene, return nan for cloud and indices
+    if any([defectr,defectg,defectb]) == True:
+        dataset = dataset.assign(mask = (dataset.blue*0)*np.nan)
+        return dataset['mask']
+    
+    # for non-defect scene
+
+    # calculating MNDWI, NDVI and NDCI spectral indices for cloud detection
+    dataset = dataset.assign(
+        MNDWI = (dataset["green"]/10000 - dataset["swir1"]/10000)/(dataset["green"]/10000 + dataset["swir1"]/10000),
+        NDVI = (dataset["nir"]/10000 - dataset["red"]/10000)/(dataset["nir"]/10000 + dataset["red"]/10000),
+        NDCI = (dataset["blue"]/10000 - dataset["nir"]/10000)/(dataset["blue"]/10000 + dataset["nir"]/10000)
+    )
+    
+    dataset = dataset.assign(
+        NDSI_exp = dataset["MNDWI"] - (0.0652 * np.exp(1.8069 * dataset["NDVI"])),
+        NDSI_div = dataset["MNDWI"] - ((dataset["NDVI"] - 0.2883) / (-0.4828)),
+        NDWI = (dataset["green"]/10000 - dataset["nir"]/10000)/(dataset["green"]/10000 + dataset["nir"]/10000),
+        NDSI_CI = (dataset["MNDWI"] - dataset["NDCI"])
+    )
+    
+    ### Check for Water ###
+    
+    water_condition1 = np.logical_and(dataset.water_vapour < 1000,dataset.red_edge3 < 100)
+    water_condition2 = np.logical_and(dataset.MNDWI > 0,np.logical_and(dataset.NDVI < 0.05,dataset.NDVI > -0.05))
+    water_condition3 = np.logical_and(dataset.MNDWI > 0,dataset.water_vapour < 1000)
+    water_condition123 = np.logical_or(np.logical_or(water_condition1, water_condition2),water_condition3)
+    dataset['water'] = xr.where(water_condition123,1,0)
+
+    if round(dataset.water.mean().values.flatten()[0],5) > 0:
+        water_presence = True
+    else:
+        water_presence = False
+        
+    if water_presence == False:
+        dataset['water'] = dataset.blue*0
+        
+    ### Check for snow ###
+    dataset['snow_presence'] = xr.where(np.logical_and(np.logical_and(dataset.red_edge3 > 4000, dataset.NDSI_CI > 0.5),dataset.MNDWI > 0.4),1,0)
+    if round(dataset.snow_presence.mean().values.flatten()[0],5) > 0:
+        snow_presence = True
+    else:
+        snow_presence = False
+         
+    # get snow classification
+    
+    if snow_presence == True:
+        snow_condition1 = np.logical_and(dataset["green"] > 4000, dataset["nir"] > 4000)
+        snow_condition2 = (dataset["MNDWI"] > 0.4)
+        snow_condition3 = np.logical_and(dataset["NDVI"] > 0.25, dataset["NDSI_exp"] >= 0)
+        snow_condition4 = np.logical_and(dataset["NDVI"] >= 0.2, 
+                                         np.logical_and(dataset["NDVI"] < 0.25, dataset["NDSI_div"] >= 0))
+        snow_condition234 = np.logical_or(np.logical_or(snow_condition2, snow_condition3), snow_condition4)
+        snow_condition = np.logical_and(snow_condition1, snow_condition234)
+
+        dataset = dataset.assign(snow = xr.where(snow_condition, 1.0, 0.0))
+    else:
+        dataset = dataset.assign(snow = dataset.blue*0)
+    
+    ### For Histogram-based Cloud Masking, all snow and water pixels are excluded before calculating histogram ###      
+    # get statistics for NDCI and water vapour distribution
+    
+    # remove nan values from array to avoid errors from calculating statistics
+    ndci_arr = dataset.where(np.logical_and(dataset.snow == 0, dataset.water == 0)).NDCI.values.flatten()
+    ndci_arr = ndci_arr[~np.isnan(ndci_arr)]
+    vapour_arr = dataset.where(np.logical_and(dataset.snow == 0, dataset.water == 0)).water_vapour.values.flatten()
+    vapour_arr = vapour_arr[~np.isnan(vapour_arr)]
+    
+    # store statistics to variables
+    NDCI_min = np.quantile(ndci_arr,0.05) # minimum NDCI allowing 5% outliners
+    NDCI_max = np.quantile(ndci_arr,0.95) # maximum NDCI allowing 5% outliners
+    NDCI_std = np.std(ndci_arr) # NDCI standard deviation
+    vapour_median = np.median(vapour_arr) # median for water vapour
+    NDCI_skew = skew(ndci_arr) # NDCI skewness: how far is NDCI away from normal distribution
+    NDCI_median = np.median(ndci_arr) # NDCI median
+    NDCI_kur = kurtosis(ndci_arr) # NDCI kurtosis: depends on the shape of distribution
+    
+    # calculating the peak of NDCI histogram and detect number of peaks
+    counts, bin_edges = np.histogram(ndci_arr) # get frequency count of every NDCI range groups
+    peaks,_ = find_peaks(counts, prominence=len(ndci_arr)/50) # get all peaks (between increasing and decreasing counts)
+    npeak = len(peaks.flatten()) # get the number of peaks
+    
+    cloudClass_condition = np.logical_and(dataset.water == 0, dataset.snow == 0)
+    
+    # Classified Scene: All Clouds
+    if NDCI_min >= -0.1:
+        dataset = dataset.assign(cloud = xr.where(dataset.NDSI_CI < 0.2, 1.0, 0.0))
+    
+    # Classified Scene: Mostly clouds
+    elif NDCI_min < -0.1 and NDCI_min >= -0.3: # high (> -0.3) NDCI for all pixels
+        if bin_edges[peaks[0]] < -0.3:
+            thres = bin_edges[peaks[0]]
+        else:
+            thres = -0.3
+
+        condition = dataset.NDCI >= thres # criteria for cloud classification
+        dataset = dataset.assign(cloud = xr.where(np.logical_and(cloudClass_condition,condition), 1.0, 0.0)) # assign cloud variable in the dataset
+        
+    # Classified Scene: Mostly clouds
+    # other condition for scene to be classify as mostly clouds: lower NDCI but histogram highly skewed towards high values
+    # kurtosis values restricted to peak to be sharp for cloudy conditions
+    # if clouds are dominant in scene, there should be only one peak in the histogram
+    elif NDCI_min < -0.3 and NDCI_median > -0.35 and NDCI_kur > -1 and NDCI_skew < 0 and npeak == 1:
+        if bin_edges[peaks[0]] < -0.3:
+            thres = bin_edges[peaks[0]]
+        else:
+            thres = -0.3
+        condition = dataset.NDCI >= thres
+        dataset = dataset.assign(cloud = xr.where(np.logical_and(cloudClass_condition,condition), 1.0, 0.0))
+    
+    # Classified Scene: Clear sky
+    # either low NDCI for all pixels or low median with really sharp peak for sunny conditionsf
+    elif NDCI_max < -0.3 or (NDCI_kur > 1.5 and NDCI_median < -0.4):
+        condition = np.logical_and(dataset.NDCI >= -0.3, dataset.water_vapour > vapour_median*1.1)
+        dataset = dataset.assign(cloud = xr.where(np.logical_and(cloudClass_condition,condition), 1.0, 0.0))
+    
+    # Classified Scene: Partly clouds
+    else:
+        cloud_ndci = dataset.where(cloudClass_condition).NDCI.values.flatten()
+        cloud_ndci = cloud_ndci[~np.isnan(cloud_ndci)]
+        counts, bin_edges = np.histogram(cloud_ndci, bins=30)
+        if npeak <= 1:
+            peak = np.argmax(counts)
+            mode = (bin_edges[peak] + bin_edges[peak+1])/2 # check dominant NDCI value
+            par = stats.percentileofscore(cloud_ndci, mode) # get quantile for dominant NDCI value
+            factor = 1 - NDCI_std # define fector to adjust NDCI threshold 
+            
+            # the more variant NDCI, the more conservative to define the end of NDCI cluster
+            adj_factor = (100 - par)*(factor/50) # tune the value to make sure percentile lies within 0% and 100%
+            refine = (par + adj_factor)/100
+            NDCI_thres = np.quantile(cloud_ndci,refine) # calculate the NDCI threshold
+
+        else:
+            min_indices = argrelmin(counts)[0]
+            ind_min = np.argmin(counts[min_indices])
+            ind_hist = min_indices[ind_min]
+            NDCI_thres = (bin_edges[ind_hist] + (-0.4))/2
+            strength = 0.5
+
+        # default values false for neg: indicate negative strength
+        neg = False
+        
+        # adjust strength depends on NDCI skewness (the proxy for cloud proportion)
+        # to make sure consistent masking independent of weather condition
+        if NDCI_skew > 1:
+            strength = strength + 0.3
+            if strength > 1:
+                strength = 1 # make sure strength lies within 0 and 1
+        elif NDCI_skew > 0 and NDCI_skew < 0.2: # less strength when there are few clouds
+            strength = strength - 0.2
+            if strength < 0:
+                neg = True
+                negative_adj = strength
+                strength = 0
+        elif NDCI_skew < -0.4: # increase strength when there are lots of clouds
+            strength = strength + 0.3
+            if strength > 1:
+                strength = 1
+            
+        # apply strength to adjust the NDCI threshold
+        if NDCI_thres < -0.4:
+            NDCI_thres_adjust = -0.4
+        elif NDCI_thres <= 0:
+            NDCI_thres_adjust = (NDCI_thres - 1) * strength * 2
+            NDCI_thres_adjust = NDCI_thres_adjust + (strength * 2)
+        elif NDCI_thres > 0:
+            NDCI_thres_adjust = (NDCI_thres + 1) * strength * -2
+            NDCI_thres_adjust = NDCI_thres_adjust - (strength * -2)
+        if neg == True:
+            NDCI_thres_adjust = NDCI_thres_adjust + negative_adj/5 
+        
+        # use NDCI sknewness as the proxy for cloudyness of the scene
+        # which is used for second condition depending on the water vapour value
+        condition1 = np.logical_and(dataset.NDCI > NDCI_thres_adjust, dataset.MNDWI < 0)
+        condition2 = np.logical_and(dataset.NDCI > NDCI_thres_adjust, dataset.NDSI_CI < 0.3)
+        condition3 = dataset.NDVI < 0.2
+        condition = np.logical_and(np.logical_or(condition1,condition2),condition3)
+        dataset = dataset.assign(cloud = xr.where(condition, 1.0, 0.0)) # add cloud variable in the dataset
+    
+    if snow_presence == True:
+        dataset["snow"] = dataset["snow"].rolling(latitude=2, longitude=2, center=True).min()
+        dataset["snow"] = dataset["snow"].rolling(latitude=2, longitude=2, center=True).max()
+        dataset["snow_buffer"] = dataset["snow"].rolling(latitude=2, longitude=2, center=True).max()
+        dataset["snow_buffer"] = dataset["snow_buffer"].fillna(0)
+
+        final_adjust = np.logical_and(dataset.cloud == 1, dataset.snow_buffer == 0)
+        dataset = dataset.assign(cloud2 = xr.where(final_adjust, 1.0, 0.0))
+        dataset["cloud"] = dataset.cloud2
+        dataset["snow"] = dataset["snow"].fillna(0)
+        
+    dataset["water"] = dataset["water"].where(np.logical_and(dataset.snow == 0,dataset.cloud == 0), 0)
+    dataset["cloud"] = dataset["cloud"].where(dataset.snow == 0, 0)
+    dataset["water"] = dataset["water"].fillna(0)
+    dataset["cloud"] = dataset["cloud"].fillna(0)
+    dataset["snow"] = dataset["snow"].fillna(0)
+    
+    # Cloud Smoothing
+    dataset["cloud_fill"] = dataset["cloud"].rolling(latitude=3, longitude=3, center=True).min()
+    dataset["cloud_fill"] = dataset["cloud_fill"].rolling(latitude=3, longitude=3, center=True).max()
+    dataset["cloud_fill"] = dataset["cloud_fill"].fillna(value = dataset.cloud)    
+    dataset["cloud"] = dataset["cloud_fill"]
+    
+    # Finalize Class
+    dataset["mask"] = dataset['snow']*11 + dataset['water']*6 + dataset['cloud']*9 
+     
+    return dataset["mask"]
+
+def ssmask_v2(dataset,strength=0.4):
     """
     Adding cloud variable to detect cloud cover in the single scene.
     Description
@@ -274,7 +526,7 @@ def ssmask_snow(dataset,strength=0.4):
         
     return dataset
 
-def ssmask(dataset,strength=0.4):
+def ssmask_v1(dataset,strength=0.4):
     """
     Adding cloud variable to detect cloud cover in the single scene.
     Description
@@ -478,8 +730,7 @@ def ssmask(dataset,strength=0.4):
         
     return dataset
 
-
-def s2cmask(ds, strength=0.4):
+def s2cmask_v3(ds, strength=0.4):
     """
     Adding cloud variable to detect cloud cover in multiple scenes.
     Description
@@ -498,11 +749,34 @@ def s2cmask(ds, strength=0.4):
         The input xr.Dataset with a new variable "cloud" of values 0, 1, or nan (0 = Not cloud, 1 = cloud, nan = defect pixel).
     """
     collection = []
-    [collection.append(ssmask(ds.sel(time = ts), strength=strength)) for ts in ds.time.to_index()]
+    [collection.append(ssmask_v3(ds.sel(time = ts), strength=strength)) for ts in ds.time.to_index()]
+    maskds = xr.concat(collection, dim = "time")
+    return maskds
+    
+def s2cmask_v2(ds, strength=0.4):
+    """
+    Adding cloud variable to detect cloud cover in multiple scenes.
+    Description
+    ----------
+    Generating cloud mask.
+    Parameters
+    ----------
+    dataset: xr.Dataset
+        dataset with mulitple time steps, including bands "water_vapour","red","green","blue","nir", and "swir1". 
+        For single time steps, use ssmask() instead.
+    stength: float
+        A number from 0 to 1 to determine the intensity for cloud masking. A higher number allows masking of less probable cloud pixels.
+    Returns
+    -------
+    dataset: xr.Dataset
+        The input xr.Dataset with a new variable "cloud" of values 0, 1, or nan (0 = Not cloud, 1 = cloud, nan = defect pixel).
+    """
+    collection = []
+    [collection.append(ssmask_v2(ds.sel(time = ts), strength=strength)) for ts in ds.time.to_index()]
     maskds = xr.concat(collection, dim = "time")
     return maskds
 
-def s2cmask_snow(ds, strength=0.4):
+def s2cmask_v1(ds, strength=0.4):
     """
     Adding cloud variable to detect cloud cover in multiple scenes.
     Description
@@ -521,6 +795,6 @@ def s2cmask_snow(ds, strength=0.4):
         The input xr.Dataset with a new variable "cloud" of values 0, 1, or nan (0 = Not cloud, 1 = cloud, nan = defect pixel).
     """
     collection = []
-    [collection.append(ssmask_snow(ds.sel(time = ts), strength=strength)) for ts in ds.time.to_index()]
+    [collection.append(ssmask_v1(ds.sel(time = ts), strength=strength)) for ts in ds.time.to_index()]
     maskds = xr.concat(collection, dim = "time")
     return maskds
