e0410423be466f1ea668d0f79a6cb3e5af8d9abd
Steve <steven.hill@uni-wuerzburg.de>
2022-10-17 16:35:43 +0200

    extract_Xy is now a independent function

--- eo2cube_tools/ml.py
+++ eo2cube_tools/ml.py
@@ -12,17 +12,56 @@ import seaborn as sns
 from sklearn.pipeline import Pipeline
 import itertools
 
+def extract_Xy(ds,feature_dims = [], labels=None, attribute=None):
+    if labels is not None:
+        if 'GeoDataFrame' in str(type(labels)):
+            if attribute is not None:
+                labels = extract_samples(ds, labels, attribute)
+            else:
+                print('Please provide the name of attribute column for rasterize')
+    if isinstance(labels, xr.Dataset):
+        raise ValueError("`labels` should be an xarray.DataArray or "
+                         "numpy array")
+    elif isinstance(labels, (xr.DataArray, np.ndarray)):
+        labels = labels.squeeze()
+
+    labels = broadcast_labels(
+        labels, ds, feature_dims)
+    if labels is not None:
+        ymask = ~np.isnan(np.array(labels))
+        np.greater(labels, 0, out=ymask, where=ymask)
+        ymask = ymask.reshape(-1)
+    else:
+        ymask = slice(None)
+
+    X = get_features(ds)[ymask]
+    Xmask = ~np.isnan(X).any(axis=1)
+    X = X[Xmask]
+
+    if labels is not None:
+        y = np.array(labels).reshape(-1)[ymask][Xmask]
+    else:
+        y = None
+
+    return X,y
 
 def rasterize(gdf,da,attribute,):    
-    crs = da.geobox.crs
-    transform = da.geobox.transform
-    dims = da.geobox.dims
-    xy_coords = [da[dims[0]], da[dims[1]]]
-    y, x = da.geobox.shape
+    if hasattr(da, 'geobox'):
+        crs = da.geobox.crs
+        transform = da.geobox.transform
+        dims = da.geobox.dims
+        xy_coords = [da[dims[0]], da[dims[1]]]
+        y, x = da.geobox.shape
+    else:
+        crs = da.odc.geobox.crs
+        transform = da.odc.geobox.transform
+        dims = da.odc.geobox.dims
+        xy_coords = [da[dims[0]], da[dims[1]]]
+        y, x = da.odc.geobox.shape
     gdf_reproj = gdf.to_crs(crs=crs)
     shapes = zip(gdf_reproj.geometry, gdf_reproj[attribute])
     arr = rasterio.features.rasterize(shapes=shapes,out_shape=(y, x),transform=transform)
-    xarr = xr.DataArray(arr,coords=xy_coords,dims=dims,attrs=da.attrs,)               
+    xarr = xr.DataArray(arr,coords=xy_coords,dims=dims,attrs=da.attrs,)                              
     return xarr
 
 def extract_samples(ds, gdf, attribute):
@@ -82,69 +121,15 @@ def get_vars_for_dims(ds, dims, invert=False):
 
 
 class ML:
-    def __init__(self, pipeline, feature_dims=[], train_size=0.8, stratify=None, to_xarray = False, vname='result'):
+    def __init__(self, pipeline, feature_dims=[], train_size=0.8, stratify=None, drop_bands=False, name='result', to_xarray = False):
         self.clf = pipeline
         self.train_size = train_size
         self.stratify = stratify
         self.feature_dims = feature_dims
-        self.vname = vname
+        self.drop_bands = drop_bands
+        self.name = name
         self.to_xarray = to_xarray
-        
-    """
-    Xarray wrapper for scikit-learn
 
-    Parameters
-    ----------
-    
-    pipeline : sklearn.pipeline.Pipeline
-        A scikit-learn pipeline. The pipeline can involve pre-processing, feature selection, classification/regression, and post-processing.
-    feature_dims : list, optional
-        A list of additional dimensions to use as features
-    train_size: int or float
-        If float, should be between 0.0 and 1.0 and represent the proportion of the dataset to include in the train split. 
-        If int, represents the absolute number of train samples. 
-    stratify: array-like
-        If not None, data is split in a stratified fashion, using this as the class labels.
-    to_xarray: bool
-        Return result as XarrayDataset
-    vname: str
-        Data Variable name for the result
-    
-    """    
-
-    def extract_Xy(self, ds, labels=None, attribute=None):
-        if labels is not None:
-            if 'GeoDataFrame' in str(type(labels)):
-                if attribute is not None:
-                    labels = extract_samples(ds, labels, attribute)
-                else:
-                    print('Please provide the name of attribute column for rasterize')
-        if isinstance(labels, xr.Dataset):
-            raise ValueError("`labels` should be an xarray.DataArray or "
-                             "numpy array")
-        elif isinstance(labels, (xr.DataArray, np.ndarray)):
-            labels = labels.squeeze()
-
-        labels = broadcast_labels(
-            labels, ds, feature_dims=self.feature_dims)
-        if labels is not None:
-            ymask = ~np.isnan(np.array(labels))
-            np.greater(labels, 0, out=ymask, where=ymask)
-            ymask = ymask.reshape(-1)
-        else:
-            ymask = slice(None)
-        
-        X = get_features(ds)[ymask]
-        Xmask = ~np.isnan(X).any(axis=1)
-        X = X[Xmask]
-        
-        if labels is not None:
-            y = np.array(labels).reshape(-1)[ymask][Xmask]
-        else:
-            y = None
-        
-        return X,y
-    
     def split(self, X, y):
         X, X_test, y, y_test = train_test_split(X, y, train_size=self.train_size, stratify=self.stratify)
         return X, X_test, y, y_test
@@ -154,14 +139,14 @@ class ML:
         return mp
     
     def train_supervised(self, ds, labels=None, attribute=None):
-        X,y = self.extract_Xy(ds, labels=labels, attribute=attribute)
+        X,y = extract_Xy(ds, feature_dims = [], labels=labels, attribute=attribute)
         self.pipe = pipeline = Pipeline(steps=self.clf)
         if self.train_size != None:
-            self.X, self.X_test, self.y, self.y_test = self.split(X, y)
+            self.X_train, self.X_test, self.y_train, self.y_test = self.split(X, y)
         else:
-            self.X = X
-            self.y = y     
-        self.model = self.pipe.fit(self.X, self.y)
+            self.X_train = X
+            self.y_train = y     
+        self.model = self.pipe.fit(self.X_train, self.y_train)
         self.mp = self.model_performance()
         
     def train_unsupervised(self, X):
@@ -184,7 +169,7 @@ class ML:
         labels_data = labels_flat.reshape(data_shape + result.shape[1:])
         labels = xr.DataArray(labels_data, dims=data_dims, coords=data_coords)
         if self.to_xarray:
-            return self.to_xr(ds, labels, self.vname)
+            return self.to_xr(ds, labels, self.drop_bands, self.name)
         else:
             return labels
         
@@ -196,25 +181,27 @@ class ML:
         for metric in metrics:
             print(f'{metric.__name__} : {metric(self.y_test, self.mp)}')
     
-    def to_xr(self, ds, labels, vname):
-        ds[vname] = labels
+    def to_xr(self, ds, labels, drop_bands, name):
+        ds[name] = labels
+        if drop_bands:
+            ds = ds[['result']]
         return ds
         
     
 class Regression(ML):
-    def __init__(self, pipeline, feature_dims=[], train_size=0.8, stratify=None, to_xarray = False, vname='result'):
-        super().__init__(pipeline, feature_dims, train_size, stratify, to_xarray, vname)
+    def __init__(self, pipeline, feature_dims=[], train_size=0.8, stratify=None, drop_bands=False, name='result', to_xarray = False):
+        super().__init__(pipeline, feature_dims, train_size, stratify, drop_bands, name, to_xarray)
     
     def train(self, ds, labels=None, attribute=None):
          return super().train_supervised(ds, labels, attribute)    
         
 class Classification(ML):
-    def __init__(self, pipeline, feature_dims=[], train_size=0.8, stratify=None, to_xarray = False, vname='result'):
-        super().__init__(pipeline, feature_dims, train_size, stratify,to_xarray, vname)
+    def __init__(self, pipeline, feature_dims=[], train_size=0.8, stratify=None, drop_bands=False, name='result', to_xarray = False):
+        super().__init__(pipeline, feature_dims, train_size, stratify, drop_bands, name, to_xarray)
     
     def train(self, ds, labels=None, attribute=None):
          return super().train_supervised(ds, labels, attribute) 
-         
+        
     def get_test_class(self):
         return np.unique(self.y_test).tolist()
         
@@ -223,17 +210,20 @@ class Classification(ML):
         if classes == None:
             classes = self.get_test_class()
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
         plt.imshow(cm, interpolation='nearest', cmap=cmap)
         plt.title(title, fontsize=30)
         plt.colorbar()
         tick_marks = np.arange(len(classes))
         plt.xticks(tick_marks, classes, rotation=45, fontsize=22)
         plt.yticks(tick_marks, classes, fontsize=22)
+
         fmt = '.2f'
         thresh = cm.max() / 2.
         for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
             plt.text(j, i, format(cm[i, j], fmt),
                      horizontalalignment="center",
                      color="white" if cm[i, j] > thresh else "black")
+
         plt.ylabel('True label', fontsize=25)
         plt.xlabel('Predicted label', fontsize=25)
